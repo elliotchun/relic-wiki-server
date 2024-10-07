a18d38cc421f7df5b53b8f63bae3ba108f7fc900
Elliot Chun <eschun@uci.edu>
2024-10-07 16:20:10 -0700

    Add styling to relics page

--- public/relics.js
+++ public/relics.js
@@ -1,8 +1,9 @@
 const response = await fetch("api/relics")
 const relics = (await response.json()).relics
-const relicContainerElement = document.getElementById("container-content")
+const relicContainerElement = document.getElementById("container-relics")
 relics.forEach(relic => {
     const relicInfo = document.createElement("div")
     relicInfo.innerText = `${relic.era} ${relic.name} (${relic.refinement})`
+    relicInfo.classList.add("container-relic")
     relicContainerElement.appendChild(relicInfo)
 })
