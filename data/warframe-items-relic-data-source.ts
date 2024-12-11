806e310e9277b713ec896f48ef9d0cbe11286bcf
Elliot Seungwoo Chun <elliotchun2@gmail.com>
2024-12-10 21:25:32 -0800

    Dependency injection of relic data sources

---
+++ data/warframe-items-relic-data-source.ts
@@ -0,0 +1,25 @@
+import { compareRelic, compareRelicReward } from "../lib/compare"
+import Items from "warframe-items"
+import type { Relic } from "warframe-items"
+import type { RelicEra, RelicRefinement, Relic as IRelic, RelicReward } from "../models/relic"
+
+const relics = new Items({ category: ["Relics"] }) as Relic[]
+
+export const warframeItemsRelics = relics
+    .map(relic => {
+        const [relicEra, relicId, relicRefinement] = relic.name.split(" ")
+        return {
+            name: relicId,
+            era: relicEra as RelicEra,
+            refinement: relicRefinement as RelicRefinement,
+            rewards: relic.rewards
+                .map(reward => ({
+                    name: reward.item.name,
+                    rarity: reward.rarity
+                } as RelicReward))
+                .sort(compareRelicReward),
+            vaulted: relic.vaulted
+        } as IRelic
+    })
+    .filter(relic => !(relic.refinement === undefined))
+    .sort(compareRelic)
---
+++ data/warframe-stat-relic-data-source.ts
@@ -0,0 +1,33 @@
+import { compareRelic, compareRelicReward } from "../lib/compare"
+import { getRarity } from "../lib/mappings"
+import type { ItemApiResponse } from "../models/relic"
+
+interface WarframeStatApiRelicReward {
+    _id: string
+    itemName: string
+    rarity: "Uncommon" | "Rare"
+    chance: number
+}
+
+interface WarframeStatApiRelic {
+    tier: "Lith" | "Neo" | "Meso" | "Axi" | "Requiem"
+    relicName: string
+    state: "Intact" | "Exceptional" | "Flawless" | "Radiant"
+    rewards: WarframeStatApiRelicReward[]
+}
+
+const response = await fetch("https://drops.warframestat.us/data/relics.json")
+const itemApiResponse = await response.json()
+export const warframeStatRelics: ItemApiResponse = itemApiResponse.relics
+    .map((apiRelic: WarframeStatApiRelic) => ({
+        name: apiRelic.relicName,
+        era: apiRelic.tier,
+        refinement: apiRelic.state,
+        rewards: apiRelic.rewards
+            .map(apiRelicReward => ({
+                name: apiRelicReward.itemName,
+                rarity: getRarity(apiRelic.state, apiRelicReward.chance)
+            }))
+            .sort(compareRelicReward)
+    }))
+    .sort(compareRelic)
